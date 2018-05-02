d9c996f31514bd98b92f5efdf8ad1a7852946eff
neal.rollins <neal-rollins@orasi.com>
2018-05-02 11:51:40 -0400

    todo-api-node: added DELETE /todos:id route

--- todo-api-node/server/server.js
+++ todo-api-node/server/server.js
@@ -7,7 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.enc.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -40,6 +40,23 @@ app.get('/todos/:id', (req, res) => {
   }
 
   Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
     if(!todo) {
       return res.status(404).send();
     }
