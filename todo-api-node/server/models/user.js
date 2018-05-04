06653388d2f81aa45880f67b6afd17e76e4c97e4
neal.rollins <neal-rollins@orasi.com>
2018-05-04 12:16:05 -0400

    todo-api-node: add POST /users/login route

--- todo-api-node/server/models/user.js
+++ todo-api-node/server/models/user.js
@@ -69,12 +69,26 @@ UserSchema.statics.findByToken = function(token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this;
 
-// bcrypt.genSalt(10, (err, salt) => {
-//   bcrypt.hash(password, salt, (err, hash) => {
-//     console.log(hash);
-//   });
-// });
+  return User.findOne({email}).then((user) => {
+    if(!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if(res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
 
 
 UserSchema.pre('save', function(next) {
